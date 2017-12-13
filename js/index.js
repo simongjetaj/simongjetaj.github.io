5b0f2cf38647c5bb056568761b28e7b046687894
Simon Gjetaj <gjetajsimon@gmail.com>
2017-12-13 17:05:04 +0100

    details

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Typewriter
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
@@ -54,3 +55,58 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.1rem solid #3E2723; padding-right: 0.4rem; text-shadow: 0.15px 0.07px 1.5px #fff; }";
         document.body.appendChild(css);
     };
+
+    // Smooth Scrolling
+    (function (window, undefined) {
+        'use strict';
+        var height_fixed_header = 82,
+            speed = 650,
+            moving_frequency = 15,
+            links = document.getElementsByTagName('a'),
+            href;
+    
+        for (var i = 0; i < links.length; i++) {
+            href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+            if (href !== null && href.length > 1 && href.indexOf('#') !== -1) {
+                links[i].onclick = function () {
+                    var element,
+                        href = this.attributes.href.nodeValue.toString(),
+                        url = href.substr(0, href.indexOf('#')),
+                        id = href.substr(href.indexOf('#') + 1);
+                    if (element = document.getElementById(id)) {
+    
+                        var hop_count = (speed - (speed % moving_frequency)) / moving_frequency,
+                            getScrollTopDocumentAtBegin = getScrollTopDocument(),
+                            gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
+    
+                        if (window.history && typeof window.history.pushState == 'function')
+                            window.history.pushState({}, undefined, url + '#' + id);
+    
+                        for (var i = 1; i <= hop_count; i++) {
+                            (function () {
+                                var hop_top_position = gap * i;
+                                setTimeout(function () { window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency * i);
+                            })();
+                        }
+    
+                        return false;
+                    }
+                };
+            }
+        }
+    
+        var getScrollTopElement = function (e) {
+            var top = height_fixed_header * -1;
+    
+            while (e.offsetParent != undefined && e.offsetParent != null) {
+                top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+                e = e.offsetParent;
+            }
+    
+            return top;
+        };
+    
+        var getScrollTopDocument = function () {
+            return window.pageYOffset !== undefined ? window.pageYOffset : document.documentElement.scrollTop !== undefined ? document.documentElement.scrollTop : document.body.scrollTop;
+        };
+    })(window);
