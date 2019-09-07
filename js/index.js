879633b8f715be39454cd8f415fbfe5ac04c8d9b
simon.gjetaj <simon.gjetaj@sparkfabrik.com>
2019-09-07 17:02:51 +0200

    Simplified smooth scrolling and small changes.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Typewriter
-var TxtType = function (el, toRotate, period) {
+var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
@@ -9,7 +9,7 @@ var TxtType = function (el, toRotate, period) {
     this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function () {
+TxtType.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
 
@@ -24,7 +24,9 @@ TxtType.prototype.tick = function () {
     var that = this;
     var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+        delta /= 2;
+    }
 
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
@@ -35,12 +37,12 @@ TxtType.prototype.tick = function () {
         delta = 500;
     }
 
-    setTimeout(function () {
+    setTimeout(function() {
         that.tick();
     }, delta);
 };
 
-window.onload = function () {
+window.onload = function() {
     var elements = document.getElementsByClassName('typewrite');
     for (var i = 0; i < elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
@@ -50,67 +52,52 @@ window.onload = function () {
         }
     }
 
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.1rem solid #fff; padding-right: 0.4rem; text-shadow: 1px 2px 1.5px #000; }";
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.innerHTML =
+        '.typewrite > .wrap { border-right: 0.1rem solid #fff; padding-right: 0.4rem; text-shadow: 1px 2px 1.5px #000; }';
     document.body.appendChild(css);
 };
 
 // Smooth Scrolling
-(function (window, undefined) {
-    'use strict';
-    var height_fixed_header = 0,
-        speed = 650,
-        moving_frequency = 15,
-        links = document.getElementsByTagName('a'),
-        href;
+const anchorLinks = ['header', 'about', 'work', 'contact'];
+const anchors = document.querySelectorAll('#navbar a');
 
-    for (var i = 0; i < links.length; i++) {
-        href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
-        if (href !== null && href.length > 1 && href.indexOf('#') !== -1) {
-            links[i].onclick = function () {
-                var element,
-                    href = this.attributes.href.nodeValue.toString(),
-                    url = href.substr(0, href.indexOf('#')),
-                    id = href.substr(href.indexOf('#') + 1);
-                if (element = document.getElementById(id)) {
+anchors.forEach((anchor, i) => {
+    anchor.addEventListener('click', function scrollToDiv(e) {
+        e.preventDefault();
 
-                    var hop_count = (speed - (speed % moving_frequency)) / moving_frequency,
-                        getScrollTopDocumentAtBegin = getScrollTopDocument(),
-                        gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
+        const ref = document.getElementById(anchorLinks[i]);
+        
+        window.scroll({
+            top: ref.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+});
 
-                    if (window.history && typeof window.history.pushState == 'function')
-                        window.history.pushState({}, undefined, url + '#' + id);
+// Navbar styling on scroll
+const navbar = document.getElementById('navbar');
+const navbarLinks = document.querySelectorAll('nav ul li a');
+const header = document.getElementById('header');
 
-                    for (var i = 1; i <= hop_count; i++) {
-                        (function () {
-                            var hop_top_position = gap * i;
-                            setTimeout(function () { window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency * i);
-                        })();
-                    }
-
-                    return false;
-                }
-            };
-        }
+window.addEventListener('scroll', e => {
+    navbar.classList.add('fixed');
+    if (window.scrollY === 0) {
+        navbarLinks.forEach(a => {
+            a.classList.remove('a_scroll');
+        });
     }
-
-    var getScrollTopElement = function (e) {
-        var top = height_fixed_header * -1;
-
-        while (e.offsetParent != undefined && e.offsetParent != null) {
-            top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
-            e = e.offsetParent;
-        }
-
-        return top;
-    };
-
-    var getScrollTopDocument = function () {
-        return window.pageYOffset !== undefined ? window.pageYOffset : document.documentElement.scrollTop !== undefined ? document.documentElement.scrollTop : document.body.scrollTop;
-    };
-})(window);
+    if (window.scrollY >= header.clientHeight) {
+        navbarLinks.forEach(a => {
+            a.classList.add('a_scroll');
+        });
+    }
+});
 
 const year = document.getElementById('year');
-const currentYear = (new Date().getFullYear());
-(currentYear === 2017) ? year.textContent = '2017' : year.textContent = `2017-${currentYear}`;
+const currentYear = new Date().getFullYear();
+currentYear === 2017
+    ? (year.textContent = '2017')
+    : (year.textContent = `2017-${currentYear}`);
